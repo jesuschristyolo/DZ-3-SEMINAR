c298a6f43ee703329177d12e4b291420648ba744
Rustam <valishev57@mail.ru>
2022-12-24 17:14:51 +0500

    complete tasks

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,7 +1,7 @@
 ﻿using static System.Console;
 
 WriteLine("Введите число палиндром");
-int numbers = int.Parse(ReadLine());
+int numbers = int.Parse(ReadLine()!);
 int a = numbers / 10000;
 int a1 = numbers % 10000;
 int b = a1 / 1000;
@@ -11,19 +11,11 @@ int d = b1 % 10;
 int d1 = c / 10;
 int e = d / 1;
 // WriteLine ($"{a} {b} {d1} {e} ");
-if (a = b )
+if (a == e && b == d1)
 {
-    
+    WriteLine ("Число является палиндромом");
 }
 else
 {
-    WriteLine ("число не является палиндромом");
-}
-if (d1 = e )
-{
-    WriteLine ("число является палиндромом");
-}
-else
-{
-    WriteLine ("число не является палиндромом");
+     WriteLine ("Число не является палиндромом");
 }
